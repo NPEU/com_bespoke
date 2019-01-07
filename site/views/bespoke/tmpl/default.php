a0bb782caa53009daee1aa8dad304f0b78c90976
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-07 11:50:04 +0000

    Added notice field and message.

--- site/views/bespoke/tmpl/default.php
+++ site/views/bespoke/tmpl/default.php
@@ -11,16 +11,16 @@ defined('_JEXEC') or die;
 
 ?>
 <div class="search<?php echo $this->pageclass_sfx; ?>">
-	<?php if ($this->params->get('show_page_heading')) : ?>
-		<h1 class="page-title">
-			<?php if ($this->escape($this->params->get('page_heading'))) : ?>
-				<?php echo $this->escape($this->params->get('page_heading')); ?>
-			<?php else : ?>
-				<?php echo $this->escape($this->params->get('page_title')); ?>
-			<?php endif; ?>
-		</h1>
-	<?php endif; ?>
-	
+    <?php if ($this->params->get('show_page_heading')) : ?>
+        <h1 class="page-title">
+            <?php if ($this->escape($this->params->get('page_heading'))) : ?>
+                <?php echo $this->escape($this->params->get('page_heading')); ?>
+            <?php else : ?>
+                <?php echo $this->escape($this->params->get('page_title')); ?>
+            <?php endif; ?>
+        </h1>
+    <?php endif; ?>
+    
     <jdoc:include type="module" name="bespoke" />
     
     <?php echo JHtml::_('content.prepare', '{loadposition bespoke}'); ?>
