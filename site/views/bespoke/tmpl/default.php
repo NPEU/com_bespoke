a6c3c3c003bcf19ba3597698a7d2f79f8da1cd44
Andy Kirk <andy.kirk@npeu.ox.ac.uk>
2019-01-07 10:16:51 +0000

    Remove dev code, update version number

--- site/views/bespoke/tmpl/default.php
+++ site/views/bespoke/tmpl/default.php
@@ -11,18 +11,24 @@ defined('_JEXEC') or die;
 
 ?>
 <div class="search<?php echo $this->pageclass_sfx; ?>">
-    <?php if ($this->params->get('show_page_heading')) : ?>
-        <h1 class="page-title">
-            <?php if ($this->escape($this->params->get('page_heading'))) : ?>
-                <?php echo $this->escape($this->params->get('page_heading')); ?>
-            <?php else : ?>
-                <?php echo $this->escape($this->params->get('page_title')); ?>
-            <?php endif; ?>
-        </h1>
-    <?php endif; ?>
-    
-    <p>Loading bespoke modules is all I do.</p>
+	<?php if ($this->params->get('show_page_heading')) : ?>
+		<h1 class="page-title">
+			<?php if ($this->escape($this->params->get('page_heading'))) : ?>
+				<?php echo $this->escape($this->params->get('page_heading')); ?>
+			<?php else : ?>
+				<?php echo $this->escape($this->params->get('page_title')); ?>
+			<?php endif; ?>
+		</h1>
+	<?php endif; ?>
+	
+    <jdoc:include type="module" name="bespoke" />
     
     <?php echo JHtml::_('content.prepare', '{loadposition bespoke}'); ?>
-
+    
+    <?php
+    $document = JFactory::getDocument();
+    $renderer   = $document->loadRenderer('modules');
+    $position   = 'bespoke';
+    $options   = array('style' => 'raw');
+    ?>
 </div>
