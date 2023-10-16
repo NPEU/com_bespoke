5d44441d8b8014da19591968269cb158bf7c2e52
Andy Kirk <andy@andykirk.net>
2023-10-16 09:20:37 +0100

    Tidy

--- site/tmpl/bespoke/default.php
+++ site/tmpl/bespoke/default.php
@@ -7,12 +7,12 @@
  * @license     MIT License; see LICENSE.md
  */
 
-#use Joomla\CMS\Language\Text;
 #use Joomla\CMS\Factory;
-#use Joomla\CMS\Router\Route;
-#use Joomla\CMS\Layout\LayoutHelper;
-#use Joomla\CMS\Layout\FileLayout;
 #use Joomla\CMS\Language\Multilanguage;
+#use Joomla\CMS\Language\Text;
+#use Joomla\CMS\Layout\FileLayout;
+#use Joomla\CMS\Layout\LayoutHelper;
+#use Joomla\CMS\Router\Route;
 #use Joomla\CMS\Session\Session;
 #use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
 
