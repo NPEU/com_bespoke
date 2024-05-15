e2b13729dc1771b8ce28634604ae566dc5ddc80d
Andy Kirk <andy@andykirk.net>
2024-05-15 15:10:00 +0100

    R

---
+++ script.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_bespoke
+ *
+ * @copyright   Copyright (C) NPEU 2023.
+ * @license     MIT License; see LICENSE.md
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\Database\DatabaseInterface;
+
+/**
+ * Removes component entry in the admin menu, as it's front-end only.
+ */
+class com_bespokeInstallerScript
+{
+    /**
+     * This method is called after a component is installed.
+     *
+     * @param  \stdClass $parent - Parent object calling this method.
+     *
+     * @return void
+     */
+    public function install($parent)
+    {
+    }
+
+    /**
+     * This method is called after a component is uninstalled.
+     *
+     * @param  \stdClass $parent - Parent object calling this method.
+     *
+     * @return void
+     */
+    public function uninstall($parent)
+    {
+    }
+
+    /**
+     * This method is called after a component is updated.
+     *
+     * @param  \stdClass $parent - Parent object calling object.
+     *
+     * @return void
+     */
+    public function update($parent)
+    {
+    }
+
+    /**
+     * Runs just before any installation action is preformed on the component.
+     * Verifications and pre-requisites should run in this function.
+     *
+     * @param  string    $type   - Type of PreFlight action. Possible values are:
+     *                           - * install
+     *                           - * update
+     *                           - * discover_install
+     * @param  \stdClass $parent - Parent object calling object.
+     *
+     * @return void
+     */
+    public function preflight($type, $parent)
+    {
+    }
+
+    /**
+     * Runs right after any installation action is preformed on the component.
+     *
+     * @param  string    $type   - Type of PostFlight action. Possible values are:
+     *                           - * install
+     *                           - * update
+     *                           - * discover_install
+     * @param  \stdClass $parent - Parent object calling object.
+     *
+     * @return void
+     */
+    public function postflight($type, $parent)
+    {
+        if ($type != 'install') {
+            return;
+        }
+
+
+        $manifest = $parent->getParent()->getManifest();
+        $name = (string) $manifest->name;
+
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true);
+        $query->select('id');
+        $query->from('#__menu');
+        $query->where('title = "' . strtoupper($name) . '_MENU"');
+        $db->setQuery($query);
+        $ids = $db->loadColumn();
+
+
+        $table = \Joomla\CMS\Factory::getApplication()->bootComponent('com_menus')->getMVCFactory()->createTable('menu', 'Administrator');
+
+        if (!empty($ids))
+        {
+            // Iterate the items to delete each one.
+            foreach ($ids as $menu_id)
+            {
+                if (!$table->delete((int) $menu_id))
+                {
+                    return false;
+                }
+            }
+            // Rebuild the whole tree
+            $table->rebuild();
+        }
+        return true;
+    }
+}
