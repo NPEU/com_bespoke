73a307b53d239557d926c9fdab8544c61716f2e1
Andy Kirk <andy@andykirk.net>
2024-05-20 13:58:02 +0100

    Create Router.php

---
+++ site/src/Service/Router.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_Bespoke
+ *
+ * @copyright   Copyright (C) NPEU 2023.
+ * @license     MIT License; see LICENSE.md
+ */
+
+namespace NPEU\Component\Bespoke\Site\Service;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\SiteApplication;
+use Joomla\CMS\Categories\CategoryFactoryInterface;
+use Joomla\CMS\Categories\CategoryInterface;
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Component\Router\RouterView;
+use Joomla\CMS\Component\Router\RouterViewConfiguration;
+use Joomla\CMS\Component\Router\Rules\MenuRules;
+use Joomla\CMS\Component\Router\Rules\NomenuRules;
+use Joomla\CMS\Component\Router\Rules\StandardRules;
+use Joomla\CMS\Menu\AbstractMenu;
+use Joomla\CMS\MVC\Factory\MVCFactoryAwareTrait;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Database\ParameterType;
+
+use NPEU\Component\Bespoke\Site\Service\CustomRouterRules;
+
+
+
+class Router extends RouterView
+{
+    use MVCFactoryAwareTrait;
+
+    private $categoryFactory;
+
+    private $categoryCache = [];
+
+    private $db;
+
+    /**
+     * Component router constructor
+     *
+     * @param   SiteApplication           $app              The application object
+     * @param   AbstractMenu              $menu             The menu object to work with
+     * @param   CategoryFactoryInterface  $categoryFactory  The category object
+     * @param   DatabaseInterface         $db               The database object
+     */
+    public function __construct(SiteApplication $app, AbstractMenu $menu)
+    {
+        //$this->categoryFactory = $categoryFactory;
+        //$this->db              = $db;
+        $this->db = \Joomla\CMS\Factory::getContainer()->get('DatabaseDriver');
+
+        $Bespoke = new RouterViewConfiguration('Bespoke');
+        $this->registerView($Bespoke);
+
+
+        parent::__construct($app, $menu);
+
+        $this->attachRule(new MenuRules($this));
+        $this->attachRule(new StandardRules($this));
+        $this->attachRule(new NomenuRules($this));
+
+    }
+}
